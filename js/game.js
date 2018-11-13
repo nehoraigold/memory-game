7e2b594a6b37f3d9de8d74ca525caa90a42abfa6
Ori Gold <nehoraigold@gmail.com>
2018-11-13 11:26:33 +0200

    working Game.board.shuffleCards method

--- js/game.js
+++ js/game.js
@@ -1,8 +1,22 @@
 Game = {
-    cards: ['a','a','b','b','c','c'],
+    cards: ['A','A','B','B','C','C','D','D','E','E','F','F'],
     cardsFlipped: 0,
+    difficulty: "easy",
+    board: {
+        cardElements: Array.from(document.getElementsByClassName('card')),
+        cardOrder: []
+    }
 }
 
-Game.newGame = function() {
-    
+Game.board.shuffleCards = function() {
+    Game.board.cardOrder = [];
+    for (var i = 0; i < Game.cards.length; i++) {
+        var ind = Math.floor(Math.random() * Game.cards.length);
+        Game.board.cardOrder.splice(ind, 0, Game.cards[i]);
+    }
+    Game.board.cardOrder = Game.board.cardOrder.filter((el) => el !== undefined);
+}
+
+Game.board.flipCard = function(event) {
+     
 }
