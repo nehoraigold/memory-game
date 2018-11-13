0a6da2664842acbaaa4f9c11a8e8b58700a04704
Ori Gold <nehoraigold@gmail.com>
2018-11-13 16:23:15 +0200

    flipping cards functionality works

--- js/game.js
+++ js/game.js
@@ -1,28 +1,54 @@
 Game = {
-    cards: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F'],
-    cardsFlipped: 0,
+    allCards: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F'],
+    cardsFound: 0,
     difficulty: "easy",
     board: {
         cardElements: Array.from(document.getElementsByClassName('card')),
-        cardOrder: []
+        cardOrder: [],
+        flippedCard: null
     }
 }
 
 Game.board.shuffleCards = function () {
     Game.board.cardOrder = [];
-    for (var i = 0; i < Game.cards.length; i++) {
-        var ind = Math.floor(Math.random() * Game.cards.length);
-        Game.board.cardOrder.splice(ind, 0, Game.cards[i]);
+    for (var i = 0; i < Game.allCards.length; i++) {
+        var ind = Math.floor(Math.random() * Game.allCards.length);
+        Game.board.cardOrder.splice(ind, 0, Game.allCards[i]);
     }
     Game.board.cardOrder = Game.board.cardOrder.filter((el) => el !== undefined);
 }
 
 Game.board.flipCard = function (event) {
-    Game.cardsFlipped++;
+    if (event.target === Game.board.flippedCard) {
+        return null;
+    }
     var cardIndex = Game.board.cardElements.indexOf(event.target);
-    event.target.style.backgroundColor = "white";
-    event.target.style.backgroundImage = "none";
-    event.target.innerHTML = Game.board.cardOrder[cardIndex];
+    event.target.className += " flipped";
+    event.target.textContent = Game.board.cardOrder[cardIndex];
+    if (Game.board.flippedCard !== null) {
+        if (event.target.textContent === Game.board.flippedCard.textContent) {
+            Game.cardsFound += 2;
+            Game.board.flippedCard = null;
+        } else {
+            setTimeout(() => {
+                event.target.innerHTML = "";
+                event.target.className = event.target.className.replace(' flipped', '');
+                Game.board.flippedCard.className = Game.board.flippedCard.className.replace(' flipped','');
+                Game.board.flippedCard.innerHTML = "";
+                Game.board.flippedCard = null;
+            }, 700);
+        }
+    } else if (Game.board.flippedCard === null) {
+        Game.board.flippedCard = event.target;
+    }
+}
+
+
+
+Game.newGame = function () {
+    Game.cardsFound = 0;
+    Game.cardsFlipped = 0;
+    Game.board.shuffleCards();
 }
 
 Game.board.bindCardActions = function () {
